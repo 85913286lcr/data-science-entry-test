d503921cff36004891a2b4b65f0d5ee38af7f449
CONGRUI LIU <85913286lcr@gmail.com>
2026-05-27 21:43:46 +0800

    q2.py

--- q2.py
+++ q2.py
@@ -7,8 +7,22 @@ def find_and_replace(lst, find_val, replace_val):
     """
     return
 
+#Answer：
+    if not isinstance(lst, list):
+        return -1
+
+    for i in range(len(lst)):
+        if lst[i] == find_val:
+            lst[i] = replace_val
+
+    return lst
+
 
 # Task 2
 # Invoke the function "find_and_replace" using the following scenarios:
 # - [1, 2, 3, 4, 2, 2], 2, 5
 # - ["apple", "banana", "apple"], "apple", "orange"
+
+#Answer：
+print(find_and_replace([1, 2, 3, 4, 2, 2], 2, 5))
+print(find_and_replace(["apple", "banana", "apple"], "apple", "orange"))
