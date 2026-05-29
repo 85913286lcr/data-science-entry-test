5e37fd62492e9232e864817a2f32ea19c8b0fc2a
CONGRUI LIU <85913286lcr@gmail.com>
2026-05-29 21:47:46 +0800

    q7.py

--- q7.py
+++ q7.py
@@ -9,7 +9,20 @@ class Car:
     def describe_car(self):
         print("")
 
+#Answer：
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def describe_car(self):
+        print(f"{self.year} {self.make} {self.model}")
 
 # Task 2
 # Create an instance of the Car class with the following attributes and call describe_car method:
 # - Make: Toyota, Model: Corolla, Year: 2020
+
+#Answer：
+car1 = Car("Toyota", "Corolla", 2020)
+car1.describe_car()
