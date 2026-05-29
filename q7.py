67445ab72937ad8a4d6ab84e5be41f0a7d5670a1
CONGRUI LIU <85913286lcr@gmail.com>
2026-05-29 22:20:24 +0800

    q7.py

--- q7.py
+++ q7.py
@@ -6,17 +6,13 @@ class Car:
     - Add a method named describe_car() that prints information about the car as "Year Make Model"
     """
 
-    def describe_car(self):
-        print("")
 
 #Answer：
-class Car:
     def __init__(self, make, model, year):
         self.make = make
         self.model = model
         self.year = year
-
-    def describe_car(self):
+   def describe_car(self):
         print(f"{self.year} {self.make} {self.model}")
 
 # Task 2
